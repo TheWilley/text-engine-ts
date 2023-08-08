66cf6d1b62b6c56a668a7fbe8ee0905470464544
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-08-08 09:25:38 +0200

    added vite for development and adjusted gulp to handle changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
-const rename = require('gulp-rename');
 const log = require('gulp-log');
 const chalk = require('chalk');
+const replace = require('gulp-replace');
 
 // Define the TypeScript project
 const tsProject = ts.createProject('tsconfig.json');
@@ -18,15 +18,13 @@ gulp.task('compile', () => {
     return tsProject.src()
         .pipe(tsProject())
         .pipe(gulp.dest('dist'))
-        .pipe(log({ message: 'TypeScript files compiled!', showFiles: false }));
 });
 
-// Task to copy HTML files
 gulp.task('copy-html', () => {
     logMessage('Copying HTML files...');
     return gulp.src('src/*.html')
+        .pipe(replace(/\.ts/g, '.js'))
         .pipe(gulp.dest('dist'))
-        .pipe(log({ message: 'HTML files copied!', showFiles: false }));
 });
 
 // Task to copy individual CSS files
@@ -34,7 +32,6 @@ gulp.task('copy-css', () => {
     logMessage('Copying CSS files...');
     return gulp.src('src/styles/*.css')
         .pipe(gulp.dest('dist/styles'))
-        .pipe(log({ message: 'CSS files copied!', showFiles: false }));
 });
 
 // Task to copy font files
@@ -42,7 +39,6 @@ gulp.task('copy-fonts', () => {
     logMessage('Copying font files...');
     return gulp.src('src/fonts/*.*')
         .pipe(gulp.dest('dist/fonts'))
-        .pipe(log({ message: 'Font files copied!', showFiles: false }));
 });
 
 // Define the build task that runs compile, copy-html, and copy-css tasks
